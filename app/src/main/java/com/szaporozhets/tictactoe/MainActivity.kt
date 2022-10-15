45d46de40a02f7e4d74862bef9821af8060c07d0
Simon <babaykalol69@gmail.com>
2022-10-15 18:49:06 +0200

    Initial commit

--- app/src/main/java/com/szaporozhets/tictactoe/MainActivity.kt
+++ app/src/main/java/com/szaporozhets/tictactoe/MainActivity.kt
@@ -3,28 +3,48 @@ package com.szaporozhets.tictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-          
+            var winningPlayer by remember {
+                mutableStateOf<Player?>(null)
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TicTacToe(
+                    onNewRound = {
+                        winningPlayer = null
+                    },
+                    onPlayerWin = {
+                        winningPlayer = it
+                    }
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+
+                winningPlayer?.let {
+                    Text(
+                        text = "Player ${it.symbol} has won!",
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
         }
     }
 }
